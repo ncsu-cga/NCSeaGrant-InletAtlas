320a4a13a657562f32c4f4d4f84a900cf762ce44
Makiko Shukunobe <m_shukunobe@hotmail.com>
2020-02-21 17:07:28 -0500

    Rename files

--- js/index.js
+++ js/index.js
@@ -1,82 +1,39 @@
 require([
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/views/SceneView",
-    "esri/widgets/Search",
-    "esri/widgets/BasemapGallery",
-    "esri/core/watchUtils",
-    // Calcite Maps
-    "calcite-maps/calcitemaps-v0.8",
+  "esri/map",
+  "esri/arcgis/utils",
+  "application/InletSwipeLayer",
+  "dojo/domReady!"
 
-    // Calcite Maps ArcGIS Support
-    "calcite-maps/calcitemaps-arcgis-support-v0.8",
+], function (
+  Map,
+  arcgisUtils,
+  InletSwipeLayer,
+) {
+    const inletsLyrUrl = 'http://services1.arcgis.com/aT1T0pU1ZdpuDk1t/arcgis/rest/services/inlets/FeatureServer/0';
+    const webmapId = '55a5665980564cfd828151e59d8ad467';
+    
+    function inletsLayerSwipe(webmapId) {
+      let params = {
+        map: null,
+        layers: null
+      };
+      let lsWidget;
 
-    // Bootstrap
-    "bootstrap/Collapse",
-    "bootstrap/Dropdown",
-    "bootstrap/Tab",
-    // Can use @dojo shim for Array.from for IE11
-    "@dojo/framework/shim/array"
-  ], function(
-    Map,
-    MapView,
-    SceneView,
-    Search,
-    Basemaps,
-    watchUtils,
-    CalciteMaps,
-    CalciteMapsArcGIS
-  ) {
-    /******************************************************************
-     *
-     * App settings
-     *
-     ******************************************************************/
+      let mapDeferred = arcgisUtils.createMap(webmapId, 'map');
+      mapDeferred.then(response => {
+        params.map = response.map;
+        params.layers = response.itemInfo.itemData.operationalLayers;
+        lsWidget = new InletSwipeLayer(params);
+        lsWidget.startup();
+      });
+    }
 
-    app = {
-      center: [-40, 40],
-      scale: 50000000,
-      basemap: "streets",
-      viewPadding: {
-        top: 50,
-        bottom: 0
-      },
-      uiComponents: ["zoom", "compass", "attribution"],
-      mapView: null,
-      containerMap: "mapViewDiv"
-    };
-
-    /******************************************************************
-     *
-     * Create the map and scene view and ui components
-     *
-     ******************************************************************/
-
-    // Map
-    const map = new Map({
-      basemap: app.basemap
+    $(window).on('load', () => {
+      $('#splash').modal('show');
     });
 
-    // 2D view
-    app.mapView = new MapView({
-      container: app.containerMap,
-      map: map,
-      center: app.center,
-      scale: app.scale,
-      padding: app.viewPadding,
-      ui: {
-        components: app.uiComponents
-      }
-    });
-
-    // CalciteMapsArcGIS.setPopupPanelSync(app.mapView);
-
-
-
-    // Create basemap widget
-    app.basemapWidget = new Basemaps({
-      view: app.activeView,
-      container: "basemapPanelDiv"
-    });
-  
-  });
+    $(document).ready(() => {
+      inletsLayerSwipe(webmapId);
+    }); 
+   
+  }); //END
